79ed4afb8a7ffa824f01c1f396e0ef4b14960a90
Miguel Pedraza <mikeped469@gmail.com>
2023-04-25 22:04:39 -0600

    Endpoint post new torneo finished

--- src/controller/torneo.controller.ts
+++ src/controller/torneo.controller.ts
@@ -1,6 +1,6 @@
 import { Torneo } from "@prisma/client";
 import { db } from "../utils/db.server";
-// import { TorneoCreate } from "../types";
+import { TorneoCreate } from "../types";
 
 export const listTorneos = async (): Promise<Torneo[]> => {  
   return db.torneo.findMany({      
@@ -14,17 +14,8 @@ export const listTorneos = async (): Promise<Torneo[]> => {
   });  
 };
 
-// export const espaciosByPadreId = async (id: number): Promise<Torneo[]> => {
-//   return db.espacio.findMany({
-//     where: {
-//       espacio_padre_id: id,
-//     },
-//   });
-// };
-
-// export const createEspacio = async (
-//   espacio: EspacioCreate
-// ): Promise<Torneo> => {
-//   return db.espacio.create({ data: espacio });
-// };
-
+export const createTorneo = async (
+  torneo: TorneoCreate
+): Promise<Torneo> => {
+  return db.torneo.create({ data: torneo });
+};
