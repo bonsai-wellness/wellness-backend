4d17b7524628cf02dee9f2238ed9ef5507831127
Miguel Pedraza <mikeped469@gmail.com>
2023-05-02 10:29:41 -0600

    Finished endpoint to delete torneo

--- src/controller/torneo.controller.ts
+++ src/controller/torneo.controller.ts
@@ -2,20 +2,26 @@ import { Torneo } from "@prisma/client";
 import { db } from "../utils/db.server";
 import { TorneoCreate } from "../types";
 
-export const listTorneos = async (): Promise<Torneo[]> => {  
-  return db.torneo.findMany({      
-    include: {
-      deporte: {
-        select: {
-          name: true
-        }
-      }
-    },
-  });  
+export const listTorneos = async (): Promise<Torneo[]> => {
+	return db.torneo.findMany({
+		include: {
+			deporte: {
+				select: {
+					name: true,
+				},
+			},
+		},
+	});
 };
 
-export const createTorneo = async (
-  torneo: TorneoCreate
-): Promise<Torneo> => {
-  return db.torneo.create({ data: torneo });
+export const createTorneo = async (torneo: TorneoCreate): Promise<Torneo> => {
+	return db.torneo.create({ data: torneo });
+};
+
+export const deleteTorneo = async (id: number): Promise<Torneo> => {
+	return db.torneo.delete({
+		where: {
+			torneo_id: id,
+		},
+	});
 };
