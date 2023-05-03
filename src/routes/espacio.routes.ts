400b0441bb9b7a763a181363e449f215038efa55
josecas <jmcg0901@gmail.com>
2023-05-02 21:53:59 -0600

    Added Blob Storage for espacio

--- src/routes/espacio.routes.ts
+++ src/routes/espacio.routes.ts
@@ -1,15 +1,31 @@
 import express from "express";
 import * as ctr from "../controller/espacio.controller";
-import { uploadImage } from "../middleware/upload.middleware";
-import { validatorPOST, validatorParams } from "../validator/espacio.validator";
+import {
+  uploadStrategy,
+  uploadToBlobStorage,
+} from "../middleware/azure.middleware";
+import {
+  validatorCreate,
+  validatorParams,
+} from "../validator/espacio.validator";
 
 const espacioRouter = express.Router();
 
 // GET routes
 espacioRouter.get("/", ctr.apiListEspacios);
-espacioRouter.get("/espacio-padre/:id", validatorParams, ctr.apiEspaciosByPadreId);
+espacioRouter.get(
+  "/espacio-padre/:id",
+  validatorParams,
+  ctr.apiEspaciosByPadreId
+);
 
 // POST routes
-espacioRouter.post("/", uploadImage, validatorPOST, ctr.apiCreateEspacio);
+espacioRouter.post(
+  "/",
+  uploadStrategy,
+  validatorCreate,
+  uploadToBlobStorage,
+  ctr.apiCreateEspacio
+);
 
 export default espacioRouter;
