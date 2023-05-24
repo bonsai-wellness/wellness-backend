ce3517c9a255b5730263528f9b7ee0dae0858366
josecas <jmcg0901@gmail.com>
2023-05-24 16:53:55 -0600

    GET time slots for a espacio complete

--- src/controller/reservation.controller.ts
+++ src/controller/reservation.controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 import * as service from "../services/reservation.service";
+import { espaciosById } from "../services/espacio.service";
 
-export const apiListAvailableSlots = async (_req: Request, res: Response) => {
+export const apiListAvailableSlots = async (req: Request, res: Response) => {
+  const ids: [number] = req.body.espacio_ids;
+  const date: Date = new Date(req.body.date);
   try {
-    const torneos = await service.listHorarios();
-    return res.status(200).json(torneos);
+    const espacios = await espaciosById(ids);
+    const timeSlots = await service.generateTimeSlotsByEspacios(espacios, date);
+    return res.status(200).json(timeSlots);
   } catch (err: any) {
     return res.status(500).json(err.message);
   }
--- src/services/reservation.service.ts
+++ src/services/reservation.service.ts
@@ -1,5 +1,85 @@
-// import { db } from "../utils/db.server";
+import { Espacio, Reservation } from "@prisma/client";
+import { db } from "../utils/db.server";
 
-export const listHorarios = async (): Promise<any> => {
-    return "Hello World"
+interface TimeSlot {
+  start_time: string;
+  end_time: string;
+}
+
+const generateTimeSlots = async (espacio: Espacio, date: Date): Promise<TimeSlot[]> => {
+  const timeSlots: TimeSlot[] = [];
+  const startTime = espacio.open_at;
+  const endTime = espacio.close_at;
+  const reservationDuration = espacio.reservation_time * 60 * 1000; // Convert minutes to milliseconds
+
+  const usedSlots = await getEspacioReservations(espacio.espacio_id, date);
+
+  // Handle scenarios where the end time is earlier than the start time
+  if (endTime.getTime() < startTime.getTime()) {
+    endTime.setDate(endTime.getDate() + 1); // Increment the end time to the next day
+  }
+
+  while (startTime.getTime() + reservationDuration <= endTime.getTime()) {
+    const slot: TimeSlot = {
+      start_time: formatTime(startTime),
+      end_time: formatTime(new Date(startTime.getTime() + reservationDuration)),
+    };
+
+    if (!isDuplicateSlot(slot, usedSlots)) {
+      timeSlots.push(slot);
+    }
+
+    startTime.setTime(startTime.getTime() + reservationDuration);
+  }
+
+  return timeSlots;
 };
+
+export const generateTimeSlotsByEspacios = async (espacios: Espacio[], date: Date) => {
+  const espaciosTimeSlots = [];
+  for (const espacio of espacios) {
+    const timeSlots = await generateTimeSlots(espacio, date);
+    const newEspacioTimeSlots = {
+      id: espacio.espacio_id,
+      timeSlots: timeSlots,
+    };
+    espaciosTimeSlots.push(newEspacioTimeSlots);
+  }
+  return espaciosTimeSlots;
+};
+
+export const getEspacioReservations = async (
+  id: number,
+  date: Date
+): Promise<Reservation[]> => {
+  // Date for setting range
+  const limitDate = new Date(date);
+  limitDate.setDate(limitDate.getDate() + 1);
+  return db.reservation.findMany({
+    where: {
+      espacio_id: id,
+      start_time: { gte: date, lt: limitDate },
+    },
+  });
+};
+
+// Helper functions
+
+function formatTime(date: Date): string {
+  return date.toTimeString().slice(0, 5);
+}
+
+function isDuplicateSlot(
+  slot: TimeSlot,
+  existingSlots: Reservation[]
+): boolean {
+  for (const existingSlot of existingSlots) {
+    if (
+      slot.start_time === formatTime(existingSlot.start_time) &&
+      slot.end_time === formatTime(existingSlot.end_time)
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
